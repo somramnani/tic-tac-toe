8c7ef67949f1024f9a355c898b0830e448435197
somramnani <som.ramnani@gmail.com>
2024-01-22 16:19:04 -0500

    add board

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(""));
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+
+  const handleClick = (e) => {
+    e.target.innerText = "X";
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {board.map((item, index) => {
+        return (
+          <div onClick={(e) => handleClick(e)} className="item">
+            {item}
+          </div>
+        );
+      })}
     </div>
   );
 }
